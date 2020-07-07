b52a1ff06f779d0abd9d58df78b889e369eced19
unknown <mateus.bezzan@aluno.unievangelica.edu.br>
2020-07-07 12:03:02 -0300

    Terceiro Modulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DiretivaNgIfComponent } from './diretiva-ng-if/diretiva-ng-if.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DiretivaNgIfComponent
   ],
   imports: [
     BrowserModule
