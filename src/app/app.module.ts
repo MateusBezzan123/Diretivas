4cdd2e22f205f6a317525361d5e1b899e69e1bde
unknown <mateus.bezzan@aluno.unievangelica.edu.br>
2020-07-08 11:23:02 -0300

    NgClass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { DiretivasNgifComponent } from './diretivas-ngif/diretivas-ngif.component';
 import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
 import { DiretivaNgforComponent } from './diretiva-ngfor/diretiva-ngfor.component';
+import { DiretivaNgclassComponent } from './diretiva-ngclass/diretiva-ngclass.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { DiretivaNgforComponent } from './diretiva-ngfor/diretiva-ngfor.componen
     DiretivasNgifComponent,
     DiretivaNgswitchComponent,
     DiretivaNgforComponent,
+    DiretivaNgclassComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
